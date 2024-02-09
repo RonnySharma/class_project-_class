8ccb72ba80a168c8c71fe53a58993f6351ce37be
Mohit Sharma <Mohit Sharma@PIYUSH>
2024-02-09 10:44:57 +0530

    company

---
+++ class_project _class/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_data.IRepositry;
+using project_data.Repository;
+using Project_model;
+
+namespace class_project__class.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly Iunitofwork _unitofwork;
+        public CompanyController(Iunitofwork unitofwork)
+        {
+            _unitofwork = unitofwork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult upsert(int? id)
+        {
+            Company company = new Company();
+            if (id == null) return View(company);
+            company = _unitofwork.Company.get(id.GetValueOrDefault());
+            if (company == null) return NotFound();
+
+
+            return View(company);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult upsert(Company company)
+        {
+            if (company == null) return NotFound();
+            if (!ModelState.IsValid) return View(company);
+            if (company.Id == 0)
+                _unitofwork.Company.Add(company);
+            else
+                _unitofwork.Company.update(company);
+            _unitofwork.save();
+            return RedirectToAction("Index");
+        }
+        #region APIs
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitofwork.Company.Getall() });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var cat = _unitofwork.Company.get(id);
+
+            if (cat == null) return Json(new
+            {
+                success = false,
+                message = "Something went !!!"
+            });
+            _unitofwork.Company.Remove(cat);
+            _unitofwork.save();
+            return Json(new { success = true, message = "Data is Delete suceess!!!" });
+        }
+        #endregion
+    }
+
+
+
+}
---
+++ class_project _class/wwwroot/js/company.js
@@ -0,0 +1,71 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+})
+function loadDataTable() {
+    dataTable = $('#tbData').DataTable({
+        "ajax": {
+            "url": "/Admin/Company/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "streetaddress", "width": "15%" },
+            { "data": "city", "width": "15%" },
+            { "data": "state", "width": "15%" },
+            { "data": "phoneno", "width": "15%" },
+            /*   { "data": "postalCode", "width": "15%" },*/
+            {
+                "data": "isAuthorizedCompany",
+                "render": function (data) {
+                    if (data)
+                        return `<input type = "checkbox" checked disabled /> `;
+                    else
+                        return `<input type = "checkbox" disabled />`;
+                }
+            },
+            {
+
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                        <a href ="/Admin/Company/upsert/${data}" class="btn btn-info">
+                        <i class="fas fa-edit"></i>
+                        </a>
+                        <a class="btn btn-danger" onclick=Delete("/Admin/Company/Delete/${data}")>
+                        <i class="fas fa-trash-alt"></i>
+                        </a>
+                        </div>
+                    
+                     `;
+                }
+
+            }
+        ]
+    })
+}
+function Delete(url) {
+    swal({
+        title: "Want to delete data ?",
+        text: "Delete Information",
+        buttons: true,
+        icon: "warning",
+        dangermodel: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
