9ad8ce8d62177c396d9fee7f8b4befd61cde2186
Mohit Sharma <Mohit Sharma@PIYUSH>
2024-02-13 10:04:19 +0530

    user

---
+++ class_project _class/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_data;
+using project_data.IRepositry;
+using project_data.Repository;
+using Project_model;
+using Projrct_unite;
+
+namespace class_project__class.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly Iunitofwork _unitowork;
+        public UserController(ApplicationDbContext context,Iunitofwork unitofwork)
+        {
+            
+           _unitowork = unitofwork;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region Api
+
+
+        [HttpGet]
+        public IActionResult Getall()
+        {
+            var userList = _context.ApplicationUsers.ToList();
+            //aspNetUseers
+            var roles = _context.Roles.ToList();
+            //aspNetRoles
+            var userRoles = _context.UserRoles.ToList();
+            //aspnetuserRoles
+            foreach (var user in userList)
+            {
+                var roleId = userRoles.FirstOrDefault(r => r.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+                if (user.CompanyId != null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = _unitowork.Company.get(Convert.ToInt32(user.CompanyId)).Name
+                    };
+                }
+                if (user.CompanyId == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+            var adminUser = userList.FirstOrDefault(u => u.Role == SD.Role_Admin);
+            userList.Remove(adminUser);
+            return Json(new { data = userList });
+        }
+        #endregion
+    }
+}
---
+++ class_project _class/wwwroot/js/user.js
@@ -0,0 +1,65 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+})
+function loadDataTable() {
+    dataTable = $('#tbldata').DataTable(
+        {
+            'ajax': {
+                "url": "/Admin/user/getall"
+            },
+            "columns": [
+                { "data": "name", "width": "15%" },
+                { "data": "email", "width": "15%" },
+                { "data": "streetAddress", "width": "15%" },
+                { "data": "company.name", "width": "15%" },
+                { "data": "role", "width": "15%" },
+                { "data": "id", "width": "10%" },
+                {
+                    "data": {
+                        id: "id", lockoutEnd: "lockoutEnd"
+                    },
+                    "render": function (data) {
+                        var today = new Date().getTime();
+                        var lockout = new Date(data.lockoutEnd).getTime();
+                        if (lockout > today) {
+                            return `
+                             <div class="text-center">
+                            <a class="btn btn-danger" onclick=LockUnLock("${data.id}")>UnLock</a>
+
+                             </div>`
+                        }
+                        else {
+                            return `<div class=text-center>
+                            <a class="btn btn-info" onclick=LockUnLock("${data.id}")>Lock</a>
+                           </div>`;
+                        }
+                    }
+                }
+
+
+
+            ]
+        }
+    )
+}
+function LockUnLock(id) {
+    $.ajax({
+        url: "/adimen/user/LockUnLock",
+        type: "POST",
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                if (data.message = "Locked") {
+                    toastr.error(data.message)
+                }
+                else {
+                    toastr.success(data.message)
+                }
+                dataTable.ajax.reload()
+            }
+        }
+
+    })
+}
